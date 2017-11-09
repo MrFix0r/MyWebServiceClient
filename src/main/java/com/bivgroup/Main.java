3b21b85f112a7377c85bdb5cb93865bd5a7ed4ba
MrFix0r <mrfix0r@gmail.com>
2017-11-09 14:41:47 +0300

    Basic HTTP auth

--- src/main/java/com/bivgroup/Main.java
+++ src/main/java/com/bivgroup/Main.java
@@ -4,13 +4,18 @@ import com.bivgroup.ws.ContractFirstWebServicePort;
 import com.bivgroup.ws.ContractFirstWebServicePortImplService;
 import com.bivgroup.ws.Faculty;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.MessageContext;
+import java.util.*;
 
 public class Main {
+    private static ContractFirstWebServicePort port;
+    private static final String WS_URL = "\t\n" +
+            "http://dubna01:8080/ContractFirstWebService-1.0-SNAPSHOT/ContractFirstWebServicePortImpl?wsdl";
+
     public static void main(String[] args) {
         ContractFirstWebServicePortImplService service = new ContractFirstWebServicePortImplService();
-        ContractFirstWebServicePort port = service.getContractFirstWebServicePortImplPort();
+        port = service.getContractFirstWebServicePortImplPort();
         System.out.println(port.getPath());
 
         System.out.println(port.getXML());
@@ -20,6 +25,20 @@ public class Main {
             System.out.println(faculty.getName());
         }
 
+        System.out.println(port.getDB());
 
+        trySendAuthRequest();
+        System.out.println(port.getPath());
+
+    }
+    public static void trySendAuthRequest() {
+
+        Map<String, Object> req_ctx = ((BindingProvider) port).getRequestContext();
+//        req_ctx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, WS_URL);
+
+        Map<String, List<String>> headers = new HashMap<String, List<String>>();
+        headers.put("Username", Collections.singletonList("user"));
+        headers.put("Password", Collections.singletonList("password"));
+        req_ctx.put(MessageContext.HTTP_REQUEST_HEADERS, headers);
     }
 }
