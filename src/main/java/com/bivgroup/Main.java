b65e904da6a45a99bb9f3bf3c0e734f30b1c09b7
MrFix0r <mrfix0r@gmail.com>
2017-11-09 17:31:38 +0300

    Добавлена SOAP Basic Auth

--- src/main/java/com/bivgroup/Main.java
+++ src/main/java/com/bivgroup/Main.java
@@ -3,38 +3,44 @@ package com.bivgroup;
 import com.bivgroup.ws.ContractFirstWebServicePort;
 import com.bivgroup.ws.ContractFirstWebServicePortImplService;
 import com.bivgroup.ws.Faculty;
+import com.bivgroup.ws.University;
 
+import javax.jws.HandlerChain;
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.handler.MessageContext;
 import java.util.*;
 
 public class Main {
+
     private static ContractFirstWebServicePort port;
-    private static final String WS_URL = "\t\n" +
-            "http://dubna01:8080/ContractFirstWebService-1.0-SNAPSHOT/ContractFirstWebServicePortImpl?wsdl";
 
     public static void main(String[] args) {
         ContractFirstWebServicePortImplService service = new ContractFirstWebServicePortImplService();
         port = service.getContractFirstWebServicePortImplPort();
-        System.out.println(port.getPath());
 
-        System.out.println(port.getXML());
-        System.out.println(port.getUni().getName());
-        List<Faculty> faculties = port.getUni().getFaculty();
+//        System.out.println(port.getXML());
+
+        University uni = port.getUni();
+        System.out.println(uni.getName());
+
+        List<Faculty> faculties = uni.getFaculty();
         for (Faculty faculty : faculties) {
             System.out.println(faculty.getName());
         }
 
-        System.out.println(port.getDB());
+//        System.out.println(port.getDB());                             //база сейчас пустая - не сработает
 
-        trySendAuthRequest();
+        //sending request before special AUTH Headers
+        System.out.println(port.getPath());
+        //adding special Headers
+        trySendAuthRequest();                       //HTTP auth
+        //sending request after addition AUTH Headers
         System.out.println(port.getPath());
 
     }
     public static void trySendAuthRequest() {
 
         Map<String, Object> req_ctx = ((BindingProvider) port).getRequestContext();
-//        req_ctx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, WS_URL);
 
         Map<String, List<String>> headers = new HashMap<String, List<String>>();
         headers.put("Username", Collections.singletonList("user"));
---
+++ src/main/java/com/bivgroup/handler/myHandler.java
@@ -0,0 +1,85 @@
+package com.bivgroup.handler;
+
+import java.io.IOException;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPConstants;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPHeaderElement;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+//public  class myHandler {
+public class myHandler implements SOAPHandler<SOAPMessageContext>{
+
+    public boolean handleMessage(SOAPMessageContext context) {
+        System.out.println("Client : handleMessage() Begin");
+        Boolean outBoundProperty  = (Boolean)
+                context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+        //If its an outgoing message from client, then outBoundProperty will be true
+        if(outBoundProperty){
+            try{
+                SOAPMessage soapMsg = context.getMessage();
+                SOAPEnvelope soapEnv = soapMsg.getSOAPPart().getEnvelope();
+                SOAPHeader soapHeader = soapEnv.getHeader();
+
+                //if no header, add the header
+                if (soapHeader == null){
+                    soapHeader = soapEnv.addHeader();
+                }
+                String macAddress = getMACAddress();
+
+                //add a soap header called "macAddress"
+                QName qname = new QName("fromClientHandler", "helloWorld");
+                SOAPHeaderElement soapHeaderElement = soapHeader.addHeaderElement(qname);
+
+                soapHeaderElement.setActor(SOAPConstants.URI_SOAP_ACTOR_NEXT);
+                //Add MAC address to SOAP header
+                soapHeaderElement.addTextNode("hello");
+                soapMsg.saveChanges();
+
+
+                //Output the message to console
+                soapMsg.writeTo(System.out);
+
+            }catch(SOAPException e){
+                System.err.println(e);
+            }catch(IOException e){
+                System.err.println(e);
+            }
+
+        }
+
+        //Returning true makes other handler chain to continue the execution
+        return true;
+    }
+
+    public boolean handleFault(SOAPMessageContext context) {
+        System.out.println("Client : handleFault() Begin");
+        return true;
+    }
+
+    public void close(MessageContext context) {
+        System.out.println("Client : close() Begin");
+
+    }
+
+    public Set<QName> getHeaders() {
+        System.out.println("Client : getHeaders() Begin");
+        return null;
+    }
+
+    private String getMACAddress(){
+        //Write a java code to get client machine MAC address and return it dynamically
+        //Returning valid MAC address
+        return "E0-DB-55-A4-10-Z4";
+
+//Returning invalid MAC address
+        //return "E0-DB-55-A4-10-Q1";
+    }
+}
